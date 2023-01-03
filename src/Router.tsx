bbd4e68dc32d91021fd31fd115fa4f2b53f6a693
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-03 12:27:33 -0300

    Correção de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Confissao } from "./pages/Confissao";
 import { Links } from "./pages/Links";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Links />} />
-
-      {/* Documentos da ICRAB */}
-      <Route path="/icrab/confissao-de-fe" element={<Confissao />} />
     </Routes>
   );
 }
